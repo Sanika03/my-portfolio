d058d7a0ee5d4814feca50db17c9adc925073044
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 01:52:09 +0530

    Add navigation bar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+const Nav = ({ menuOpen, toggleMenu }) => {
+  return (
+    <nav className={`text-grape bg-cream p-4 fixed top-0 right-0 left-0 z-50 ${menuOpen ? 'opacity-30' : ''}`}>
+      <div className="flex items-center justify-between">
+        <div className="text-2xl font-bold">
+          Sanika Suryawanshi
+        </div>
+        {/* Menu Button */}
+        <div className="cursor-pointer" onClick={() => toggleMenu()}>
+          {
+            menuOpen ?
+            null:
+            <FontAwesomeIcon icon={faBars} size="lg" />
+          }
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
