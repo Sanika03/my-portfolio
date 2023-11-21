98cf7908afe0388c00c649918aeccdfd3a77c059
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 01:51:41 +0530

    Add custom styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ultra-red": "#EF798A",
+        "cream": "#FFF8E7",
+        "blue-purple": "#7D82BB",
+        "grape": "#613F75",
+        "queen-pink": "#E5C3D1"
+      }
+    },
   },
   plugins: [],
 }
-
