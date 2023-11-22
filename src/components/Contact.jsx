f1647a3fa3b965d7aa31ea32fa1a8766bc033470
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 14:19:05 +0530

    Add text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,11 +9,10 @@ const Contact = () => {
       <section id="contact" className="h-screen bg-queen-pink p-8 text-grape flex justify-center items-center">
         <div className="max-w-2xl mx-auto text-center">
           <h2 className="text-3xl font-bold mb-8">CONTACT</h2>
-
+          <p className="text-lg font-semibold mb-5">| I AM OPEN TO WORK AND NETWORK |</p>
           <p className="mb-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula mauris eu urna aliquet, eu tincidunt odio hendrerit.
+            If you have an exciting project in mind, want to collaborate, or simply wish to connect, let's initiate a conversation. 
           </p>
-
           <div className="flex items-center justify-center">
             <a href="mailto:sanikasuryawanshi0305@gmail.com" className="mr-4">
               <FontAwesomeIcon icon={faEnvelope} size="lg" className='hover:text-black'/>
