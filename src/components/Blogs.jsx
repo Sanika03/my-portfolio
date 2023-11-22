f15d5901659bfc5079834c46a1f43e886327d384
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 14:18:56 +0530

    Add blog details

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -5,9 +5,9 @@ const Blogs = () => {
   const blogs = [
     {
       id: 1,
-      title: "Blog Title 1",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vehicula mauris eu urna aliquet, eu tincidunt odio hendrerit.",
-      link: "https://example.com/blog1",
+      title: "Javascript errors: ReferenceError v/s TypeError",
+      content: "A programmer has to write 1000s of lines of code and it is impossible to write 100% error-free code in one go. This article will help you avoid often-made errors in Javascript...",
+      link: "https://sanika.hashnode.dev/javascript-errors-referenceerror-vs-typeerror",
     },
   ];
 
@@ -18,9 +18,9 @@ const Blogs = () => {
           <h2 className="text-3xl font-archivo font-bold mb-8 text-grape">BLOG</h2>
           {blogs.map(({ id, title, content, link }) => (
             <div key={id} className="bg-cream text-grape p-6 rounded-md shadow-md mb-4">
-              <h3 className="text-xl font-bold mb-2">{title}</h3>
-              <p className="text-sm mb-4">{content}</p>
-              <a href={link} className="text-grape py-2 px-4 rounded-md underline hover:opacity-60">Visit</a>
+              <h3 className="text-[22px] font-bold mb-4">{title}</h3>
+              <p className="text-base font-medium mb-4">{content}</p>
+              <a href={link} target="_blank" rel="noopener noreferrer" className="text-grape py-2 px-4 rounded-md underline hover:opacity-60">Read more</a>
             </div>
           ))}
         </div>
