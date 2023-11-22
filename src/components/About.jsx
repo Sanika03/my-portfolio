098c08e6ff8922fec828c014338c3e6d73e6ff75
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 10:50:18 +0530

    Modify styling and change text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,15 @@ const About = () => {
   return (
     <Element name="about">
       <section id="about" className="h-screen bg-gradient-to-r from-blue-purple to-queen-pink p-8 text-grape flex justify-center items-center">
-        <div className="max-w-2xl mx-auto text-center">
+        <div className="max-w-3xl mx-auto text-center">
           <div>
-            <h2 className="text-4xl font-bold mb-5 border-b-2 border-white pb-5">ABOUT ME</h2>
-            <p className="text-lg">
-              I am a passionate frontend developer with a keen interest in technology since childhood. Eager to contribute my
-              skills and enthusiasm to a dynamic team, I bring a fresh perspective and a commitment to delivering engaging and
-              user-friendly web experiences.
-            </p>
+            <h2 className="text-3xl font-bold mb-6 border-b-2 border-white pb-6">CRAFTING IDEAS INTO STUNNING WEB REALITIES</h2>
+            <div className="text-lg font-medium flex flex-col gap-4">
+            <p>Passionate about frontend development since my early days, I bring a tech-driven journey to your team.</p>
+            <p>Known for my precision and a commitment to perfection, I tackle challenges head-on, always figuring things out even in the unknown.</p>
+            <p>As a team player with a friendly demeanor, I seamlessly fit into any collaborative environment.</p>
+            <p>Let's connect and craft remarkable web experiences together!</p>
+            </div>
           </div>
         </div>
       </section>
