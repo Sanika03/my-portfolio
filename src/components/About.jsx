cc5d72b02ccb53f4899898358b40b20769484a2f
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 12:20:12 +0530

    Change title font family and size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <section id="about" className="h-screen bg-gradient-to-r from-blue-purple to-queen-pink p-8 text-grape flex justify-center items-center">
         <div className="max-w-3xl mx-auto text-center">
           <div>
-            <h2 className="text-3xl font-bold mb-6 border-b-2 border-white pb-6">CRAFTING IDEAS INTO STUNNING WEB REALITIES</h2>
+            <h2 className="text-[26px] font-archivo font-bold mb-6 border-b-2 border-white pb-6">CRAFTING IDEAS INTO STUNNING WEB REALITIES</h2>
             <div className="text-lg font-medium flex flex-col gap-4">
             <p>Passionate about frontend development since my early days, I bring a tech-driven journey to your team.</p>
             <p>Known for my precision and a commitment to perfection, I tackle challenges head-on, always figuring things out even in the unknown.</p>
