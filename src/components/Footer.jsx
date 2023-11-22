cff19ef8c3d3add967a5aced76e0753f6b90e326
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 12:19:34 +0530

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="text-grape text-center py-7">
+            <p className='font-bold pb-2'>Crafted with ❤️ by Sanika Suryawanshi</p>
+            <a href="https://github.com/Sanika03/my-portfolio" className="underline hover:opacity-70">View code on GitHub</a>
+        </div>
+    )
+}
+
+export default Footer;
