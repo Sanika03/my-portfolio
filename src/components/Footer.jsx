4a739a9532a0c6bda4b1bbaad80f0d9787dc69e7
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 12:32:13 +0530

    feat: Add security measures to external link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ const Footer = () => {
     return (
         <div className="text-grape text-center py-7">
             <p className='font-bold pb-2'>Crafted with ❤️ by Sanika Suryawanshi</p>
-            <a href="https://github.com/Sanika03/my-portfolio" className="underline hover:opacity-70">View code on GitHub</a>
+            <a 
+            href="https://github.com/Sanika03/my-portfolio" 
+            target="_blank"
+            rel="noopener noreferrer" 
+            className="underline hover:opacity-70">View code on GitHub</a>
         </div>
     )
 }
