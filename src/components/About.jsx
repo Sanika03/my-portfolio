030d0e407cd9f8d13eb5921521b141a814c425fe
Sanika03 <sanikasuryawanshi0305@gmail.com>
2023-11-22 10:28:13 +0530

    Change font size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <section id="about" className="h-screen bg-gradient-to-r from-blue-purple to-queen-pink p-8 text-grape flex justify-center items-center">
         <div className="max-w-2xl mx-auto text-center">
           <div>
-            <h2 className="text-3xl font-bold mb-5 border-b-2 border-white pb-5">ABOUT ME</h2>
+            <h2 className="text-4xl font-bold mb-5 border-b-2 border-white pb-5">ABOUT ME</h2>
             <p className="text-lg">
               I am a passionate frontend developer with a keen interest in technology since childhood. Eager to contribute my
               skills and enthusiasm to a dynamic team, I bring a fresh perspective and a commitment to delivering engaging and
