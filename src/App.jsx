475fa8eeb4e9d0b5948d29b90ea97828fb7c2d53
Sanika03 <sanikasuryawanshi0305@gmail.com>
2026-06-11 04:06:53 +0530

    Reorder components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import Home from './components/Home';
-import About from './components/About';
-import Skills from './components/Skills';
-import Blogs from './components/Blogs';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Menu from './components/Menu';
-import videoBackground from '../public/assets/background.mp4';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Experience from "./components/Experience";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Menu from "./components/Menu";
+import videoBackground from "../public/assets/background.mp4";
+import Footer from "./components/Footer";
 
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -18,15 +17,15 @@ const App = () => {
   };
 
   const sectionStyle = {
-    position: 'relative',
-    overflow: 'hidden',
+    position: "relative",
+    overflow: "hidden",
   };
 
   const videoStyle = {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover',
-    position: 'fixed',
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    position: "fixed",
     top: 0,
     left: 0,
     zIndex: -1,
@@ -36,21 +35,21 @@ const App = () => {
     <div className="font-montserrat">
       <Nav menuOpen={menuOpen} toggleMenu={toggleMenu} />
 
-      <main className={`mt-[64px] relative ${menuOpen && 'opacity-80'}`} style={sectionStyle}>
+      <main
+        className={`mt-[64px] relative ${menuOpen && "opacity-80"}`}
+        style={sectionStyle}
+      >
         <video autoPlay loop muted playsInline style={videoStyle}>
           <source src={videoBackground} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <Home />
-        <About />
-        <Skills />
         <Projects />
-        <Blogs />
+        <Experience />
+        <Skills />
         <Contact />
-        <Footer/>
+        <Footer />
       </main>
-
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} toggleMenu={toggleMenu} />
     </div>
   );
 };
